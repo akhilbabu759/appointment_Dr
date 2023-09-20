3e54da4d0706fb8330a2e66278d9ef80e6c692d9
akhil babu <85557134+akhilbabu759@users.noreply.github.com>
2023-09-21 01:04:55 +0530

    lock orinattion added

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,30 @@
 import 'package:drbook/screen/appointment/view/appointment.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
 }
-class MyApp extends StatelessWidget {
+void _portraitModeOnly() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+}
+mixin PortraitModeMixin on StatelessWidget  {
+  @override
+  Widget build(BuildContext context) {
+    _portraitModeOnly();
+    return Text('') ;
+  }
+}
+class MyApp extends StatelessWidget with PortraitModeMixin{
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     return const GetMaterialApp(debugShowCheckedModeBanner: false,
       home: Appointment(),
     );
